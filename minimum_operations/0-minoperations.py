b4f0d72fcdc986999b9d369268f10996b0cef9b7
iheb-drihmi <ihebdrihmi2002@gmail.com>
2024-01-23 16:55:56 +0100

    tasks

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,36 +1,32 @@
 #!/usr/bin/python3
-"""Minimum Operations"""
+
+
+"""
+Calculates the minimum operations
+"""
 
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations required to
-    obtain exactly n 'H' characters in the file.
+    Calculate the fewest number of operations needed to reach exactly n H characters.
 
     Args:
-        n (int): The target number of 'H' characters.
+    - n (int): Target number of H characters.
 
     Returns:
-        int: The minimum number of operations needed.
+    - int: The fewest number of operations needed.
+           If n is impossible to achieve, returns 0.
     """
-
     if n <= 1:
         return 0
-
+    
     operations = 0
+    divisor = 2
 
-    while n % 2 == 0:
-        operations += 2
-        n //= 2
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
 
-    i = 3
-    while i * i <= n:
-        while n % i == 0:
-            operations += i
-            n //= i
-        i += 2
-
-    if n > 2:
-        operations += n
-
-    return int(operations)
+    return operations
